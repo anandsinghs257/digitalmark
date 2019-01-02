3bb11df61cdebf5854e8860608e53ef33535461a
anandsinghs257 <anandsinghs257@gmail.com>
2019-01-02 20:15:55 +0530

    migration generated

--- db/migrate/20190102144317_create_users.rb
+++ db/migrate/20190102144317_create_users.rb
@@ -2,11 +2,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :full_name
-      t.integer :contact_no
+      t.string :contact_no
       t.string :email
       t.text :message
-
-      t.timestamps
     end
   end
 end
